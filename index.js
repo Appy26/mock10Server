a547782e7f2a0639eeb6f33ce50a1f9e3a73a579
Appy26 <aparnaharsh2604@gmail.com>
2023-06-08 14:08:41 +0530

    chnaged

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const authenticate = require('./middlewares/authenticate');
 const app = express();
 app.use(express.json())
 app.use(cors())
-app.use("/users",userRoute)
+app.use("/users", userRoute)
 app.use(authenticate)
-app.listen(process.env.port, async() => {
+app.listen(process.env.port, async () => {
   try {
-      await connection
-      console.log("server is running")
+    await connection
+    console.log("server is running")
+    console.log("connected to db")
   } catch (error) {
-   console.log(error.message);
+    console.log(error.message);
   }
 })
