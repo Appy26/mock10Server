7c309ed0a258276ebfe7bb4f9cf39ea7b76564e0
Appy26 <aparnaharsh2604@gmail.com>
2023-06-08 14:01:15 +0530

    nock

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoute = require('./Routes/user.routes');
 const cors = require('cors');
 const { connection } = require("./config/db");
+require("dotenv").config();
 const authenticate = require('./middlewares/authenticate');
 
 const app = express();
